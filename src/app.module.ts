efe6df3a020afcf9311d94270c000136721d5359
TongLongDev <tonglong.dev@gmail.com>
2026-04-18 19:19:06 +0700

    log middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { SongsController } from './songs/songs.controller';
 import { SongsModule } from './songs/songs.module';
 
 @Module({
@@ -8,4 +10,8 @@ import { SongsModule } from './songs/songs.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes(SongsController);
+  }
+}
