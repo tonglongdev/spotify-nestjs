6bd1ae161e08a2455825f1c7557279184b8cbf46
TongLongDev <tonglong.dev@gmail.com>
2026-04-19 12:04:20 +0700

    DevConfigService

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { DevConfigService } from './common/providers/DevConfigService';
 
 @Injectable()
 export class AppService {
+  constructor(
+    private devConfigService: DevConfigService,
+    @Inject('CONFIG')
+    private config: { port: string },
+  ) {}
   getHello(): string {
-    return 'Hello World!';
+    return `Hello I am learning Nest.js Fundamentals ${this.devConfigService.getDBHOST()} PORT = ${
+      this.config.port
+    }`;
   }
 }
